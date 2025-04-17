bb564c7103422492ef9af5abfbc2776f4aa2678d
Gabriel McKay <gabriel.p.mckay@gmail.com>
2025-04-17 10:58:27 -0700

    ai stations can theoretically trade once trains are connected

--- game/Cargo/Cargo_Objects/Specific/Player/ai_station.gd
+++ game/Cargo/Cargo_Objects/Specific/Player/ai_station.gd
@@ -5,10 +5,12 @@ const TRADE_MARGINS: float = 1.1
 var local_market: Dictionary[int, int]
 var local_market_prices: Dictionary[int, float]
 
-var global_market: Dictionary[int, int]
-var global_market_prices: Dictionary[int, float]
+var requests: Dictionary[int, Array] #Type to array of requests
+#Represents amount of local goods already commited to request
+var committed_sales: Dictionary[int, int] #Type -> Amount
 
-var connected_stations: Dictionary[Vector2i, int] #Set of coords, int represents dist
+#Represents downstream stations from this station
+var ds_stations: Dictionary[Vector2i, int] #Set of coords, int represents dist
 
 #TODO: Basically sell whatever you have in hold, if price good
 #TODO: Buy if it knows it can sell it for higher, or if connected station logic
@@ -19,15 +21,13 @@ func get_local_market() -> Dictionary[int, int]:
 func get_local_market_prices() -> Dictionary[int, float]:
 	return local_market_prices
 
+func get_requests() -> Dictionary[int, Array]:
+	return requests
+
 func reset_local_market() -> void:
 	for type: int in terminal_map.get_number_of_goods():
 		local_market[type] = 0
 		local_market_prices[type] = 0.0
-
-func reset_global_market() -> void:
-	for type: int in terminal_map.get_number_of_goods():
-		global_market[type] = 0
-		global_market_prices[type] = 0.0
 
 func month_tick() -> void:
 	update_all_markets()
@@ -37,8 +37,7 @@ func month_tick() -> void:
 func update_all_markets() -> void:
 	reset_local_market()
 	fetch_local_market()
-	reset_global_market()
-	fetch_global_market()
+	update_orders()
 
 func fetch_local_market() -> void:
 	for tile: Vector2i in connected_terminals:
@@ -50,50 +49,56 @@ func fetch_local_market() -> void:
 		local_market_prices[type] /= local_market[type]
 
 func add_orders_to_local_market(orders: Dictionary, dist: int) -> void:
-	add_orders_to_market(orders, dist, true)
-
-#This function usually will fetch a month old market and sometimes get current market
-func fetch_global_market() -> void:
-	for tile: Vector2i in connected_stations:
-		var station_obj: ai_station = terminal_map.get_ai_station(tile)
-		if station_obj != null:
-			station_obj.get_orders()
-			add_orders_to_global_market(station_obj.get_orders(), 1)
-	for type: int in global_market_prices:
-		global_market_prices[type] /= global_market[type]
-
-func add_orders_to_global_market(orders: Dictionary, dist: int) -> void:
-	add_orders_to_market(orders, dist, false)
-
-func add_orders_to_market(orders: Dictionary, dist: int, local: bool) -> void:
 	for order: trade_order in orders.values():
 		var type: int = order.get_type()
 		var amount: int = order.get_amount()
 		if dist != 1:
 			amount = min(amount, AUTO_ROAD_LOAD_TICK_AMOUNT)
-		if local:
-			local_market[type] += amount
-			local_market_prices[type] += order.get_max_price() * amount
-		else:
-			global_market[type] += amount
-			global_market_prices[type] += order.get_max_price() * amount
-
-#func add_to_global_market(p_local_market: Dictionary[int, int], p_local_market_prices: Dictionary[int, float]) -> void:
-	#for type: int in p_local_market:
-		#global_market[type] += p_local_market[type]
-		#global_market_prices[type] += p_local_market_prices[type]
+		local_market[type] += amount
+		local_market_prices[type] += order.get_max_price() * amount
 
 #TODO: Use global market and local market to make orders
 func update_orders() -> void:
 	update_buy_orders()
 
 func update_buy_orders() -> void:
-	for type: int in local_market:
-		var price_mult: float = local_market_prices[type] / global_market_prices[type]
-		#Positive Trade, set to take at least 10% margins
-		#TODO: Change this number to fit with amount traveled by rail, eg: include expenses
-		if price_mult > TRADE_MARGINS:
-			update_buy_order(type, global_market[type], global_market_prices[type] / TRADE_MARGINS)
+	for tile: Vector2i in ds_stations:
+		var ds_station: ai_station = terminal_map.get_ai_station(tile)
+		var ds_requests: Dictionary[int, Array] = ds_station.get_requests()
+		for type: int in ds_requests:
+			for req: request in ds_requests[type]:
+				update_buy_orders_from_station(req)
+
+func update_buy_orders_from_station(req: request) -> void:
+	var type: int = req.type
+	#Represents looping request
+	if req.source == location:
+		return
+	
+	#Do price checking before seeing if it will fill request or repeat
+	if local_market_prices[type] * TRADE_MARGINS < req.max_price:
+		var amount_avail: int = local_market[type] - committed_sales[type]
+		var amount_sourced_globally: int = 0
+		if req.amount > amount_avail:
+			committed_sales[type] = local_market[type]
+			update_buy_order(type, local_market[type], req.max_price)
+			amount_sourced_globally = req.amount - amount_avail
+		else:
+			#More than enough to fill req
+			committed_sales[type] += req.amount
+			update_buy_order(type, committed_sales[type], req.max_price)
+		
+		#Deal with global sourced, pass on
+		update_request(type, req.source, amount_sourced_globally, req.max_price)
+	else:
+		update_request(type, req.source, req.amount, req.max_price)
+
+func update_request(p_type: int, p_source: Vector2i, p_amount: int, p_max_price: float) -> void:
+	for req: request in requests[p_type]:
+		if req.source == p_source:
+			req.amount = p_amount
+			return
+	requests[p_type].append(request.new(p_type, p_amount, p_source, p_max_price))
 
 func update_buy_order(type: int, amount: int, max_price: float) -> void:
 	edit_order(type, amount, true, max_price)
---
+++ game/Cargo/Cargo_Objects/request.gd
@@ -0,0 +1,12 @@
+class_name request extends Node
+
+var type: int
+var amount: int
+var source: Vector2i
+var max_price: float
+
+func _init(p_type: int, p_amount: int, p_source: Vector2i, p_max_price: float) -> void:
+	type = p_type
+	amount = p_amount
+	source = p_source
+	max_price = p_max_price
