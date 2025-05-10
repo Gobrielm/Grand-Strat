aed491b1f673bbf039fc630b1c1e557ccf2bcf35
Gabriel McKay <gabriel.p.mckay@gmail.com>
2025-05-10 15:16:59 -0700

    working on ai stations

--- game/Cargo/Cargo_Objects/Specific/Player/ai_station.gd
+++ game/Cargo/Cargo_Objects/Specific/Player/ai_station.gd
@@ -3,8 +3,41 @@ class_name ai_station extends station
 const TRADE_MARGINS: float = 1.1
 const TIME_UNTIL_RESET_DS_STATION: int = 100
 
-var local_market: Dictionary[int, int]
-var local_market_prices: Dictionary[int, float]
+class local_market:
+	var input_market: Dictionary[int, int] #Type -> Amount
+	var output_market: Dictionary[int, int] #Type -> Amount
+	var local_market_prices: Dictionary[int, float] #Type -> Price
+	
+	func _init() -> void:
+		#Inits the market as well
+		reset_local_market()
+	
+	func reset_local_market() -> void:
+		for type: int in terminal_map.get_number_of_goods():
+			input_market[type] = 0
+			output_market[type] = 0
+			local_market_prices[type] = 0.0
+	
+	func normalize_local_market() -> void:
+		#Before it is the price * amount
+		for type: int in local_market_prices:
+			local_market_prices[type] /= (input_market[type] + output_market[type])
+	
+	func add_orders_to_local_market(orders: Dictionary) -> void:
+		for order: trade_order in orders.values():
+			var type: int = order.get_type()
+			var amount: int = order.get_amount()
+			if order.is_buy_order():
+				output_market[type] += amount
+			else:
+				input_market[type] += amount
+			#Does take max, but makes sure that some margins are guarenteed
+			local_market_prices[type] += order.get_limit_price() * amount
+	
+	func get_price(type: int) -> float:
+		return local_market_prices[type]
+
+var market: local_market
 
 var requests: Dictionary[int, Dictionary] #Type to Dictionary[Vector2i, Array[request]]
 #Represents amount of local goods already commited to request
@@ -17,19 +50,12 @@ var timer: Dictionary[int, int] = {} #Set Train_id -> Time until it erases ds_st
 #TODO: Basically sell whatever you have in hold, if price good
 #TODO: Buy if it knows it can sell it for higher, or if connected station logic
 
-func get_local_market() -> Dictionary[int, int]:
-	return local_market
-
-func get_local_market_prices() -> Dictionary[int, float]:
-	return local_market_prices
-
+func _init(p_location: Vector2i, p_owner: int) -> void:
+	super._init(p_location, p_owner)
+	market = local_market.new()
+	
 func get_requests() -> Dictionary[int, Dictionary]:
 	return requests
-
-func reset_local_market() -> void:
-	for type: int in terminal_map.get_number_of_goods():
-		local_market[type] = 0
-		local_market_prices[type] = 0.0
 
 func day_tick() -> void:
 	super.day_tick()
@@ -44,7 +70,7 @@ func month_tick() -> void:
 
 #Thread unsafe
 func update_all_markets() -> void:
-	reset_local_market()
+	market.reset_local_market()
 	fetch_local_market()
 	update_orders()
 
@@ -52,20 +78,8 @@ func fetch_local_market() -> void:
 	for tile: Vector2i in connected_terminals:
 		var broker_obj: broker = terminal_map.get_broker(tile)
 		if broker_obj != null:
-			var dist: int = connected_terminals[tile]
-			add_orders_to_local_market(broker_obj.get_orders(), dist)
-	for type: int in local_market_prices:
-		local_market_prices[type] /= local_market[type]
-
-func add_orders_to_local_market(orders: Dictionary, dist: int) -> void:
-	for order: trade_order in orders.values():
-		var type: int = order.get_type()
-		var amount: int = order.get_amount()
-		#If can only trade by road, limit amount
-		if dist != 1:
-			amount = min(amount, MAX_SUPPLY_GIVEN)
-		local_market[type] += amount
-		local_market_prices[type] += order.get_limit_price() * amount
+			market.add_orders_to_local_market(broker_obj.get_orders())
+	market.normalize_local_market()
 
 #Will be run every go around to reset timer
 func add_ds_station(train_id: int, future_station: Vector2i) -> void:
@@ -82,6 +96,7 @@ func update_orders() -> void:
 	update_sell_orders()
 
 func update_buy_orders() -> void:
+	update_domestic_buy_orders()
 	for tile: Vector2i in ds_stations.values():
 		var ds_station: ai_station = terminal_map.get_ai_station(tile)
 		var ds_requests: Dictionary[int, Dictionary] = ds_station.get_requests()
@@ -89,29 +104,38 @@ func update_buy_orders() -> void:
 			for req: request in ds_requests[type].values():
 				update_buy_orders_from_station(req)
 
+func update_domestic_buy_orders() -> void:
+	var input_market: Dictionary[int, int] = market.input_market
+	var output_market: Dictionary[int, int] = market.output_market
+	for type: int in input_market:
+		#Make sure inputs needed outweight amount created
+		if input_market[type] > output_market[type]:
+			#Create request with difference, since the rest can domestic
+			update_request(type, location, input_market[type] - output_market[type], market.get_price(type))
+
 func update_buy_orders_from_station(req: request) -> void:
 	var type: int = req.type
 	#Represents looping request
 	if req.source == location:
 		return
-	
+	#TODO: LOTS OF FIXES AROUND REQUESTS AND BUY/SELLING
 	#Do price checking before seeing if it will fill request or repeat
-	if local_market_prices[type] * TRADE_MARGINS < req.max_price:
-		var amount_avail: int = local_market[type] - committed_sales[type]
-		var amount_sourced_globally: int = 0
-		if req.amount > amount_avail:
-			committed_sales[type] = local_market[type]
-			update_buy_order(type, local_market[type], req.max_price)
-			amount_sourced_globally = req.amount - amount_avail
-		else:
-			#More than enough to fill req
-			committed_sales[type] += req.amount
-			update_buy_order(type, committed_sales[type], req.max_price)
-		
-		#Deal with global sourced, pass on
-		update_request(type, req.source, amount_sourced_globally, req.max_price)
-	else:
-		update_request(type, req.source, req.amount, req.max_price)
+	#if local_market_prices[type] * TRADE_MARGINS < req.max_price:
+		#var amount_avail: int = local_market[type] - committed_sales[type]
+		#var amount_sourced_globally: int = 0
+		#if req.amount > amount_avail:
+			#committed_sales[type] = local_market[type]
+			#update_buy_order(type, local_market[type], req.max_price)
+			#amount_sourced_globally = req.amount - amount_avail
+		#else:
+			##More than enough to fill req
+			#committed_sales[type] += req.amount
+			#update_buy_order(type, committed_sales[type], req.max_price)
+		#
+		##Deal with global sourced, pass on
+		#update_request(type, req.source, amount_sourced_globally, req.max_price)
+	#else:
+		#update_request(type, req.source, req.amount, req.max_price)
 
 func update_request(p_type: int, p_source: Vector2i, p_amount: int, p_max_price: float) -> void:
 	if requests[p_type].has(p_source):
@@ -123,5 +147,6 @@ func update_buy_order(type: int, amount: int, max_price: float) -> void:
 	edit_order(type, amount, true, max_price)
 
 func update_sell_orders() -> void:
-	for type: int in local_market:
-		update_request(type, location, local_market[type], local_market_prices[type])
+	var output_market: Dictionary[int, int] = market.output_market
+	for type: int in output_market:
+		update_request(type, location, output_market[type], market.get_price(type))
