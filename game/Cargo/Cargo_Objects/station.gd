a64718b06fd9f5c34af190979bcb175826e20503
Gabriel McKay <gabriel.p.mckay@gmail.com>
2025-06-01 18:00:58 -0700

    finished the gdscript implementation of station

--- game/Cargo/Cargo_Objects/station.gd
+++ game/Cargo/Cargo_Objects/station.gd
@@ -1,66 +1,11 @@
-class_name station extends broker
-
-var MAX_SUPPLY_DISTANCE: int = 5
-var MAX_SUPPLY_GIVEN: int = 5
-const SUPPLY_DROPOFF: int = 1
-
-func _init(new_location: Vector2i, _player_owner: int) -> void:
-	super._init(new_location, _player_owner)
-	#Uses it to keep track of attempts and change, not for price
-	local_pricer = local_price_controller.new({}, {}) 
-
-#Gets the local market's price
-func get_local_price(type: int) -> float:
-	var amount_total: int = 0
-	var market_price: float = 0.0 #Starts as domestic output becomes price
-	for tile: Vector2i in connected_terminals:
-		var brok: broker = terminal_map.get_instance().get_broker(tile)
-		if brok == null:
-			continue
-		var order: trade_order = brok.get_order(type)
-		if order == null:
-			continue
-		amount_total += order.amount
-		market_price += order.amount * brok.get_local_price(type)
-			
-	return market_price / amount_total
-
-func place_order(type: int, amount: int, buy: bool, max_price: float) -> void:
-	add_accept(type)
-	local_pricer.add_cargo_type(type)
-	super.place_order(type, amount, buy, max_price)
-
-func edit_order(type: int, amount: int, buy: bool, max_price: float) -> void:
-	if trade_orders.has(type):
-		super.edit_order(type, amount, buy, max_price)
-	else:
-		place_order(type, amount, buy, max_price)
-
-func get_orders_magnitude() -> int:
-	var tot: int = 0
-	for order: trade_order in trade_orders.values():
-		tot += order.amount
-	return tot
-
-func remove_order(type: int) -> void:
-	if trade_orders.has(type):
-		trade_orders[type].queue_free()
-		local_pricer.remove_cargo_type(type)
-		remove_accept(type)
-		trade_orders.erase(type)
-
-func distribute_cargo() -> void:
-	#Prioritize re-supplying friendly units
-	supply_armies()
-	for order: trade_order in trade_orders.values():
-		if order.is_sell_order():
-			distribute_from_order(order)
+class_name Station extends StationWOMethods
 
 func supply_armies() -> void:
 	var units_to_supply: Dictionary[Vector2i, int] = get_units_to_supply()
 	if units_to_supply.size() > 0:
 		print("supplying unit")
 	for tile: Vector2i in units_to_supply:
+		var storage: Dictionary = get_current_hold()
 		for type: int in storage:
 			if storage[type] == 0:
 				continue
@@ -70,8 +15,8 @@ func get_units_to_supply() -> Dictionary[Vector2i, int]:
 	var map: TileMapLayer = Utils.world_map
 	var unit_map: TileMapLayer = Utils.unit_map
 	var visited: Dictionary = {}
-	visited[location] = MAX_SUPPLY_DISTANCE
-	var queue: Array = [location]
+	visited[get_location()] = MAX_SUPPLY_DISTANCE
+	var queue: Array = [get_location()]
 	var units_to_supply: Dictionary[Vector2i, int] = {}
 	while !queue.is_empty():
 		var curr: Vector2i = queue.pop_front()
@@ -100,29 +45,3 @@ func supply_unit(unit: base_unit, type: int, max_supply: int) -> void:
 	var amount: int = transfer_cargo(type, desired)
 	org.add_cargo(type, amount)
 	#TODO: Do some storing of, amount spent/goods used, on war
-
-func add_connected_terminal(new_terminal: terminal) -> void:
-	super.add_connected_terminal(new_terminal)
-	update_accepts_from_trains()
-
-func remove_connected_terminal(new_terminal: terminal) -> void:
-	super.remove_connected_terminal(new_terminal)
-	update_accepts_from_trains()
-
-func update_accepts_from_trains() -> void:
-	reset_accepts_train()
-	for coords: Vector2i in connected_terminals:
-		var obj: terminal = terminal_map.get_instance().get_terminal(coords)
-		if obj is fixed_hold:
-			add_accepts(obj)
-
-func add_accepts(obj: terminal) -> void:
-	for index: int in terminal_map.get_instance().get_number_of_goods():
-		if obj.does_accept(index):
-			add_accept(index)
-
-func reset_accepts_train() -> void:
-	reset_accepts()
-
-func day_tick() -> void:
-	distribute_cargo()
---
+++ game/Cargo/Cargo_Objects/stationOLD.gd
@@ -0,0 +1,128 @@
+class_name station extends broker
+
+var MAX_SUPPLY_DISTANCE: int = 5
+var MAX_SUPPLY_GIVEN: int = 5
+const SUPPLY_DROPOFF: int = 1
+
+func _init(new_location: Vector2i, _player_owner: int) -> void:
+	super._init(new_location, _player_owner)
+	#Uses it to keep track of attempts and change, not for price
+	local_pricer = local_price_controller.new({}, {}) 
+
+#Gets the local market's price
+func get_local_price(type: int) -> float:
+	var amount_total: int = 0
+	var market_price: float = 0.0 #Starts as domestic output becomes price
+	for tile: Vector2i in connected_terminals:
+		var brok: broker = terminal_map.get_instance().get_broker(tile)
+		if brok == null:
+			continue
+		var order: trade_order = brok.get_order(type)
+		if order == null:
+			continue
+		amount_total += order.amount
+		market_price += order.amount * brok.get_local_price(type)
+			
+	return market_price / amount_total
+
+func place_order(type: int, amount: int, buy: bool, max_price: float) -> void:
+	add_accept(type)
+	local_pricer.add_cargo_type(type)
+	super.place_order(type, amount, buy, max_price)
+
+func edit_order(type: int, amount: int, buy: bool, max_price: float) -> void:
+	if trade_orders.has(type):
+		super.edit_order(type, amount, buy, max_price)
+	else:
+		place_order(type, amount, buy, max_price)
+
+func get_orders_magnitude() -> int:
+	var tot: int = 0
+	for order: trade_order in trade_orders.values():
+		tot += order.amount
+	return tot
+
+func remove_order(type: int) -> void:
+	if trade_orders.has(type):
+		trade_orders[type].queue_free()
+		local_pricer.remove_cargo_type(type)
+		remove_accept(type)
+		trade_orders.erase(type)
+
+func distribute_cargo() -> void:
+	#Prioritize re-supplying friendly units
+	supply_armies()
+	for order: trade_order in trade_orders.values():
+		if order.is_sell_order():
+			distribute_from_order(order)
+
+func supply_armies() -> void:
+	var units_to_supply: Dictionary[Vector2i, int] = get_units_to_supply()
+	if units_to_supply.size() > 0:
+		print("supplying unit")
+	for tile: Vector2i in units_to_supply:
+		for type: int in storage:
+			if storage[type] == 0:
+				continue
+			supply_army(tile, type, units_to_supply[tile])
+
+func get_units_to_supply() -> Dictionary[Vector2i, int]:
+	var map: TileMapLayer = Utils.world_map
+	var unit_map: TileMapLayer = Utils.unit_map
+	var visited: Dictionary = {}
+	visited[location] = MAX_SUPPLY_DISTANCE
+	var queue: Array = [location]
+	var units_to_supply: Dictionary[Vector2i, int] = {}
+	while !queue.is_empty():
+		var curr: Vector2i = queue.pop_front()
+		for tile: Vector2i in map.get_surrounding_cells(curr):
+			if !visited.has(tile) or (visited.has(tile) and visited[tile] < visited[curr] - SUPPLY_DROPOFF):
+				#Can't supply through enemy units
+				if unit_map.tile_has_enemy_army(tile, player_owner):
+					continue
+				visited[tile] = visited[curr] - SUPPLY_DROPOFF
+				if visited[tile] > 0:
+					queue.push_back(tile)
+					if unit_map.tile_has_friendly_army(tile, player_owner):
+						units_to_supply[tile] = visited[tile]
+	return units_to_supply
+
+func supply_army(tile: Vector2i, type: int, max_supply: int) -> void:
+	var army_obj: army = Utils.unit_map.get_army(tile, player_owner)
+	if army_obj == null:
+		return
+	for unit: base_unit in army_obj.get_units():
+		supply_unit(unit, type, max_supply)
+	
+func supply_unit(unit: base_unit, type: int, max_supply: int) -> void:
+	var org: organization = unit.org
+	var desired: int = min(org.get_desired_cargo(type), max_supply)
+	var amount: int = transfer_cargo(type, desired)
+	org.add_cargo(type, amount)
+	#TODO: Do some storing of, amount spent/goods used, on war
+
+func add_connected_terminal(new_terminal: terminal) -> void:
+	super.add_connected_terminal(new_terminal)
+	update_accepts_from_trains()
+
+func remove_connected_terminal(new_terminal: terminal) -> void:
+	super.remove_connected_terminal(new_terminal)
+	update_accepts_from_trains()
+
+func update_accepts_from_trains() -> void:
+	reset_accepts_train()
+	for coords: Vector2i in connected_terminals:
+		var obj: terminal = terminal_map.get_instance().get_terminal(coords)
+		if obj is fixed_hold:
+			add_accepts(obj)
+
+func add_accepts(obj: terminal) -> void:
+	for index: int in terminal_map.get_instance().get_number_of_goods():
+		if obj.does_accept(index):
+			add_accept(index)
+
+func reset_accepts_train() -> void:
+	reset_accepts()
+
+func day_tick() -> void:
+	distribute_cargo()
