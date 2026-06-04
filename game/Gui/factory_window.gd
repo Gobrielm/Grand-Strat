ecd06ea9ae3b050d6c8cd457651b81a1a0484738
Gabriel McKay <gabriel.p.mckay@gmail.com>
2026-06-03 17:03:00 -0700

    changes to factory window

--- game/Gui/factory_window.gd
+++ game/Gui/factory_window.gd
@@ -2,7 +2,6 @@ extends Window
 var location: Variant = null
 var hold_name: String
 
-var current_cargo: Dictionary
 var town_pdps: Dictionary
 
 var current_cash: int
@@ -10,14 +9,17 @@ var current_level: int
 var type_hovering: int = -1
 var inside_price_list: bool = false
 
+var unformatted_factory_information: Dictionary = {}
+
 const time_every_update: int = 1
 var progress: float = 0.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hide()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
+	if !visible:
+		return
 	progress += delta
 	if progress > time_every_update:
 		refresh_window()
@@ -34,10 +36,9 @@ func open_window(new_location: Vector2i) -> void:
 func refresh_window() -> void:
 	progress = 0
 	if location != null:
-		request_current_cargo.rpc_id(1, location)
 		request_current_name.rpc_id(1, location)
 		request_current_prices.rpc_id(1, location)
-		request_current_basic_labels.rpc_id(1, location)
+		request_factory_information.rpc_id(1, location)
 
 func refresh_hover() -> void:
 	if inside_price_list:
@@ -54,11 +55,6 @@ func start_hovering_type(type: int) -> void:
 		type_hovering = type
 
 @rpc("any_peer", "call_local", "unreliable")
-func request_current_cargo(coords: Vector2i) -> void:
-	var dict: Dictionary = ProvinceManager.get_instance().get_factory_info(coords)
-	update_current_cargo.rpc_id(multiplayer.get_remote_sender_id(), dict)
-
-@rpc("any_peer", "call_local", "unreliable")
 func request_current_name(coords: Vector2i) -> void:
 	var current_name: String = map_data.get_instance().get_hold_name(coords)
 	update_current_name.rpc_id(multiplayer.get_remote_sender_id(), current_name)
@@ -69,29 +65,9 @@ func request_current_prices(coords: Vector2i) -> void:
 	update_current_prices.rpc_id(multiplayer.get_remote_sender_id(), dict)
 
 @rpc("any_peer", "call_local", "unreliable")
-func request_current_basic_labels(coords: Vector2i) -> void:
-	pass
-	#var broker: Broker = TerminalMap.get_instance().get_broker(coords)
-	#var _current_cash: int = TerminalMap.get_instance().get_cash_of_firm(coords)
-	#var level: int = 0
-	#if broker is FactoryTemplate:
-		#level = (TerminalMap.get_instance().get_broker(coords) as FactoryTemplate).get_level_without_employment()
-	#update_current_cash.rpc_id(multiplayer.get_remote_sender_id(), _current_cash)
-	#update_current_level.rpc_id(multiplayer.get_remote_sender_id(), level)
-	#update_connected_status.rpc_id(multiplayer.get_remote_sender_id(), broker.get_number_of_connected_terminals())
-
-@rpc("any_peer", "call_local", "unreliable")
-func request_current_level(coords: Vector2i) -> void:
-	#var broker: Broker = TerminalMap.get_instance().get_broker(coords)
-	var _current_level: int = 0
-	#if broker is FactoryTemplate:
-		#_current_level = (TerminalMap.get_instance().get_broker(coords) as FactoryTemplate).get_level_without_employment()
-	update_current_level.rpc_id(multiplayer.get_remote_sender_id(), _current_level)
-
-@rpc("authority", "call_local", "unreliable")
-func update_current_cargo(new_current_cargo: Dictionary) -> void:
-	current_cargo = new_current_cargo
-	factory_window()
+func request_factory_information(coords: Vector2i) -> void:
+	var unformatted_info: Dictionary = ProvinceManager.get_instance().get_factory_info(coords)
+	update_factory_infromation.rpc_id(multiplayer.get_remote_sender_id(), unformatted_info)
 
 @rpc("authority", "call_local", "unreliable")
 func update_current_name(new_name: String) -> void:
@@ -109,25 +85,29 @@ func update_current_prices(new_prices: Dictionary) -> void:
 	display_current_prices()
 
 @rpc("authority", "call_local", "unreliable")
-func update_current_level(new_level: int) -> void:
-	current_level = new_level
-	$Level.text = "Level: " + str(current_level)
+func update_connected_status(connections: int) -> void:
+	$Connected.text = "Connections: " + str(connections)
 
 @rpc("authority", "call_local", "unreliable")
-func update_connected_status(connections: int) -> void:
-	$Connected.text = "Connections: " + str(connections);
+func update_factory_infromation(unformatted_info: Dictionary) -> void:
+	unformatted_factory_information = unformatted_info
+	$Cash.text = "Cash: " + str(unformatted_info.get("cash", 0))
+	$Level.text = "Level: " + str(unformatted_info.get("level", 0))
+	$UnemploymentRate.text = "U-rate: " + str(100.0 - unformatted_info.get("employment_rate", 0) * 100) + "%"
+	factory_window()
 
 func factory_window() -> void:
 	var cargo_list: ItemList = $Cargo_Node/Cargo_List
 	var names: Array = CargoInfo.get_instance().get_cargo_array()
-	var selected_name: String = get_selected_name()
+	#var selected_name: String = get_selected_name()
 	
 	for i: int in cargo_list.item_count:
 		cargo_list.remove_item(0)
-	for type: int in current_cargo:
-		if current_cargo[type].amount != 0:
+	var cargo_dict: Dictionary = unformatted_factory_information.get("cargo", {})
+	for type: int in cargo_dict:
+		if cargo_dict[type].amount != 0:
 			var cargo_name: String = names[type]
-			cargo_list.add_item(cargo_name + ", " + str(current_cargo[type].amount))
+			cargo_list.add_item(cargo_name + ", " + str(cargo_dict[type].amount))
 			#if cargo_name == selected_name:
 				#cargo_list.select(type)
 
@@ -160,22 +140,19 @@ func _on_price_list_mouse_exited() -> void:
 	$CargoInfoPopUp.stop_hover()
 
 func _on_cargo_info_pop_up_popup_requested() -> void:
-	populate_info_window.rpc_id(1, type_hovering, location)
+	populate_info_window(type_hovering)
 
-@rpc("any_peer", "call_local", "unreliable")
-func populate_info_window(type: int, p_location: Vector2i) -> void:
-	var unformatted_info: Dictionary = ProvinceManager.get_instance().get_factory_info(p_location)
-	if (!unformatted_info.has(type)): return
+func populate_info_window(type: int) -> void:
+	if (!unformatted_factory_information.has(type)): return
 	
 	var info: Dictionary = {}
 	info.type = CargoInfo.get_instance().get_cargo_name(type)
-	info.price = "$" + str(Utils.round(unformatted_info[type].price, 2))
-	info.supply = "Supply: " + str(unformatted_info[type].supply)
-	info.demand = "Demand: " + str(unformatted_info[type].demand)
+	info.price = "$" + str(Utils.round(unformatted_factory_information[type].price, 2))
+	info.supply = "Supply: " + str(unformatted_factory_information[type].supply)
+	info.demand = "Demand: " + str(unformatted_factory_information[type].demand)
 	
-	pop_up_info_window.rpc_id(multiplayer.get_remote_sender_id(), info)
+	pop_up_info_window(info)
 
-@rpc("authority", "call_local", "unreliable")
 func pop_up_info_window(info: Dictionary) -> void:
 	var pop_up: cargo_info_popup = $CargoInfoPopUp
 	pop_up.pop_up_info_window(info, get_mouse_position() + Vector2(position))
